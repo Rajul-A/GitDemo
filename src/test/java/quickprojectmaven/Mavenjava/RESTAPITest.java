e75c4af5284b61038c0843a67aaaa6207ddbd016
Rajul-A <rajul.agrawal1612@gamil.com>
2021-06-21 14:07:15 +0530

    first commit

---
+++ src/test/java/quickprojectmaven/Mavenjava/RESTAPITest.java
@@ -0,0 +1,17 @@
+package quickprojectmaven.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	@Test(description="This is testcase1")  
+
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+}
